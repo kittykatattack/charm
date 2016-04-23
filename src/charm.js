577843ccf3242729b59c9afbea656707331bddfa
Rex <dandylion13@gmail.com>
2016-04-23 11:38:01 -0400

    Fixed a bug where sprites don't always make it to their destination due to a slow frame rate or duration.

--- src/charm.js
+++ src/charm.js
@@ -1,7 +1,7 @@
 export class Charm {
   constructor(renderingEngine = PIXI) {
 
-    if (renderingEngine === undefined) throw new Error("Please assign a rendering engine in the constructor before using charm.js"); 
+    if (renderingEngine === undefined) throw new Error("Please assign a rendering engine in the constructor before using charm.js");
 
     //Find out which rendering engine is being used (the default is Pixi)
     this.renderer = "";
@@ -10,7 +10,7 @@ export class Charm {
     if (renderingEngine.ParticleContainer && renderingEngine.Sprite) {
       this.renderer = "pixi";
     }
-    
+
 
     //An array to store the global tweens
     this.globalTweens = [];
@@ -19,31 +19,59 @@ export class Charm {
     this.easingFormulas = {
 
       //Linear
-      linear(x) {return x;},
+      linear(x) {
+        return x;
+      },
 
       //Smoothstep
-      smoothstep(x) {return x * x * (3 - 2 * x);},
-      smoothstepSquared(x) {return Math.pow((x * x * (3 - 2 * x)), 2);},
-      smoothstepCubed(x) {return Math.pow((x * x * (3 - 2 * x)), 3);},
+      smoothstep(x) {
+        return x * x * (3 - 2 * x);
+      },
+      smoothstepSquared(x) {
+        return Math.pow((x * x * (3 - 2 * x)), 2);
+      },
+      smoothstepCubed(x) {
+        return Math.pow((x * x * (3 - 2 * x)), 3);
+      },
 
       //Acceleration
-      acceleration(x) {return x * x;},
-      accelerationCubed(x) {return Math.pow(x * x, 3);},
+      acceleration(x) {
+        return x * x;
+      },
+      accelerationCubed(x) {
+        return Math.pow(x * x, 3);
+      },
 
       //Deceleration
-      deceleration(x) {return 1 - Math.pow(1 - x, 2);},
-      decelerationCubed(x) {return 1 - Math.pow(1 - x, 3);},
+      deceleration(x) {
+        return 1 - Math.pow(1 - x, 2);
+      },
+      decelerationCubed(x) {
+        return 1 - Math.pow(1 - x, 3);
+      },
 
       //Sine
-      sine(x) {return Math.sin(x * Math.PI / 2);},
-      sineSquared(x) {return Math.pow(Math.sin(x * Math.PI / 2), 2);},
-      sineCubed(x) {return Math.pow(Math.sin(x * Math.PI / 2), 2);},
-      inverseSine(x) {return 1 - Math.sin((1 - x) * Math.PI / 2);},
-      inverseSineSquared(x) {return 1 - Math.pow(Math.sin((1 - x) * Math.PI / 2), 2);},
-      inverseSineCubed(x) {return 1 - Math.pow(Math.sin((1 - x) * Math.PI / 2), 3);},
+      sine(x) {
+        return Math.sin(x * Math.PI / 2);
+      },
+      sineSquared(x) {
+        return Math.pow(Math.sin(x * Math.PI / 2), 2);
+      },
+      sineCubed(x) {
+        return Math.pow(Math.sin(x * Math.PI / 2), 2);
+      },
+      inverseSine(x) {
+        return 1 - Math.sin((1 - x) * Math.PI / 2);
+      },
+      inverseSineSquared(x) {
+        return 1 - Math.pow(Math.sin((1 - x) * Math.PI / 2), 2);
+      },
+      inverseSineCubed(x) {
+        return 1 - Math.pow(Math.sin((1 - x) * Math.PI / 2), 3);
+      },
 
       //Spline
-      spline (t, p0, p1, p2, p3) {
+      spline(t, p0, p1, p2, p3) {
         return 0.5 * (
           (2 * p1) +
           (-p0 + p2) * t +
@@ -56,10 +84,7 @@ export class Charm {
       cubicBezier(t, a, b, c, d) {
         let t2 = t * t;
         let t3 = t2 * t;
-        return a  
-          + (-a * 3 + t * (3 * a - a * t)) * t
-          + (3 * b + t * (-6 * b + b * 3 * t)) * t 
-          + (c * 3 - c * 3 * t) * t2 + d * t3;
+        return a + (-a * 3 + t * (3 * a - a * t)) * t + (3 * b + t * (-6 * b + b * 3 * t)) * t + (c * 3 - c * 3 * t) * t2 + d * t3;
       }
     };
 
@@ -69,20 +94,26 @@ export class Charm {
         if (!sprite.scaleX && sprite.scale.x) {
           Object.defineProperty(
             sprite,
-            "scaleX", 
-            {
-              get() {return sprite.scale.x}, 
-              set(value) {sprite.scale.x = value} 
+            "scaleX", {
+              get() {
+                return sprite.scale.x
+              },
+              set(value) {
+                sprite.scale.x = value
+              }
             }
           );
         }
         if (!sprite.scaleY && sprite.scale.y) {
-          Object.defineProperty( 
+          Object.defineProperty(
             sprite,
-            "scaleY", 
-            {
-              get() {return sprite.scale.y}, 
-              set(value) {sprite.scale.y = value} 
+            "scaleY", {
+              get() {
+                return sprite.scale.y
+              },
+              set(value) {
+                sprite.scale.y = value
+              }
             }
           );
         }
@@ -93,19 +124,19 @@ export class Charm {
   //The low level `tweenProperty` function is used as the foundation
   //for the the higher level tween methods.
   tweenProperty(
-    sprite,                  //Sprite object
-    property,                //String property
-    startValue,              //Tween start value
-    endValue,                //Tween end value
-    totalFrames,             //Duration in frames
-    type = "smoothstep",     //The easing type
-    yoyo = false,            //Yoyo?
-    delayBeforeRepeat = 0    //Delay in frames before repeating
+    sprite, //Sprite object
+    property, //String property
+    startValue, //Tween start value
+    endValue, //Tween end value
+    totalFrames, //Duration in frames
+    type = "smoothstep", //The easing type
+    yoyo = false, //Yoyo?
+    delayBeforeRepeat = 0 //Delay in frames before repeating
   ) {
 
     //Create the tween object
     let o = {};
-    
+
     //If the tween is a bounce type (a spline), set the
     //start and end magnitude values
     let typeArray = type.split(" ");
@@ -137,7 +168,7 @@ export class Charm {
     //The `update` method will be called on each frame by the game loop.
     //This is what makes the tween move
     o.update = () => {
-      
+
       let time, curvedTime;
 
       if (o.playing) {
@@ -156,8 +187,8 @@ export class Charm {
           //If it's not a spline, use one of the ordinary easing functions
           if (typeArray[0] !== "bounce") {
             curvedTime = this.easingFormulas[type](normalizedTime);
-          } 
-          
+          }
+
           //If it's a spline, use the `spline` function and apply the
           //2 additional `type` array values as the spline's start and
           //end points
@@ -173,11 +204,14 @@ export class Charm {
 
         //When the tween has finished playing, run the end tasks
         else {
-          o.end(); 
+
+          // make sure we end on the actual end property
+          sprite[property] = o.endValue;
+          o.end();
         }
       }
     };
-      
+
     //The `end` method will be called when the tween is finished
     o.end = () => {
 
@@ -203,7 +237,7 @@ export class Charm {
     //Pause and play methods
     o.play = () => o.playing = true;
     o.pause = () => o.playing = false;
-    
+
     //Return the tween object
     return o;
   }
@@ -222,7 +256,7 @@ export class Charm {
 
     //Make a new tween for each array
     tweensToAdd.forEach(tweenPropertyArguments => {
-      
+
       //Use the tween property arguments to make a new tween
       let newTween = this.tweenProperty(...tweenPropertyArguments);
 
@@ -233,7 +267,7 @@ export class Charm {
     //Add a counter to keep track of the
     //number of tweens that have completed their actions
     let completionCounter = 0;
-    
+
     //`o.completed` will be called each time one of the tweens
     //finishes
     o.completed = () => {
@@ -247,13 +281,13 @@ export class Charm {
         if (o.onComplete) o.onComplete();
         completionCounter = 0;
       }
-    }; 
+    };
 
     //Add `onComplete` methods to all tweens
     o.tweens.forEach(tween => {
       tween.onComplete = () => o.completed();
     });
-    
+
     //Add pause and play methods to control all the tweens
     o.pause = () => {
       o.tweens.forEach(tween => {
@@ -301,10 +335,10 @@ export class Charm {
   //2. Complex tweens
 
   slide(
-    sprite, endX, endY, 
+    sprite, endX, endY,
     frames = 60, type = "smoothstep", yoyo = false, delayBeforeRepeat = 0
   ) {
-    return this.makeTween([ 
+    return this.makeTween([
 
       //Create the x axis tween
       [sprite, "x", sprite.x, endX, frames, type, yoyo, delayBeforeRepeat],
@@ -316,7 +350,7 @@ export class Charm {
   }
 
   breathe(
-    sprite, endScaleX = 0.8, endScaleY = 0.8, 
+    sprite, endScaleX = 0.8, endScaleY = 0.8,
     frames = 60, yoyo = true, delayBeforeRepeat = 0
   ) {
 
@@ -327,13 +361,13 @@ export class Charm {
 
       //Create the scaleX tween
       [
-        sprite, "scaleX", sprite.scaleX, endScaleX, 
+        sprite, "scaleX", sprite.scaleX, endScaleX,
         frames, "smoothstepSquared", yoyo, delayBeforeRepeat
       ],
 
       //Create the scaleY tween
       [
-        sprite, "scaleY", sprite.scaleY, endScaleY, 
+        sprite, "scaleY", sprite.scaleY, endScaleY,
         frames, "smoothstepSquared", yoyo, delayBeforeRepeat
       ]
     ]);
@@ -344,24 +378,24 @@ export class Charm {
     //Add `scaleX` and `scaleY` properties to Pixi sprites
     this._addScaleProperties(sprite);
 
-    return this.makeTween([ 
+    return this.makeTween([
 
       //Create the scaleX tween
       [
-        sprite, "scaleX", sprite.scaleX, endScaleX, 
+        sprite, "scaleX", sprite.scaleX, endScaleX,
         frames, "smoothstep", false
       ],
 
       //Create the scaleY tween
       [
-        sprite, "scaleY", sprite.scaleY, endScaleY, 
+        sprite, "scaleY", sprite.scaleY, endScaleY,
         frames, "smoothstep", false
       ]
     ]);
   }
 
   strobe(
-    sprite, scaleFactor = 1.3, startMagnitude = 10, endMagnitude = 20, 
+    sprite, scaleFactor = 1.3, startMagnitude = 10, endMagnitude = 20,
     frames = 10, yoyo = true, delayBeforeRepeat = 0
   ) {
 
@@ -370,30 +404,30 @@ export class Charm {
     //Add `scaleX` and `scaleY` properties to Pixi sprites
     this._addScaleProperties(sprite);
 
-    return this.makeTween([ 
+    return this.makeTween([
 
       //Create the scaleX tween
       [
-        sprite, "scaleX", sprite.scaleX, scaleFactor, frames, 
+        sprite, "scaleX", sprite.scaleX, scaleFactor, frames,
         bounce, yoyo, delayBeforeRepeat
       ],
 
       //Create the scaleY tween
       [
-        sprite, "scaleY", sprite.scaleY, scaleFactor, frames, 
+        sprite, "scaleY", sprite.scaleY, scaleFactor, frames,
         bounce, yoyo, delayBeforeRepeat
       ]
     ]);
   }
 
   wobble(
-    sprite, 
-    scaleFactorX = 1.2, 
-    scaleFactorY = 1.2, 
+    sprite,
+    scaleFactorX = 1.2,
+    scaleFactorY = 1.2,
     frames = 10,
-    xStartMagnitude = 10, 
+    xStartMagnitude = 10,
     xEndMagnitude = 10,
-    yStartMagnitude = -10, 
+    yStartMagnitude = -10,
     yEndMagnitude = -10,
     friction = 0.98,
     yoyo = true,
@@ -406,17 +440,17 @@ export class Charm {
     //Add `scaleX` and `scaleY` properties to Pixi sprites
     this._addScaleProperties(sprite);
 
-    let o = this.makeTween([ 
+    let o = this.makeTween([
 
       //Create the scaleX tween
       [
-        sprite, "scaleX", sprite.scaleX, scaleFactorX, frames, 
+        sprite, "scaleX", sprite.scaleX, scaleFactorX, frames,
         bounceX, yoyo, delayBeforeRepeat
       ],
 
       //Create the scaleY tween
       [
-        sprite, "scaleY", sprite.scaleY, scaleFactorY, frames, 
+        sprite, "scaleY", sprite.scaleY, scaleFactorY, frames,
         bounceY, yoyo, delayBeforeRepeat
       ]
     ]);
@@ -432,7 +466,7 @@ export class Charm {
           //Set the `endValue` to 1 when the effect is finished and 
           //remove the tween from the global `tweens` array
           if (tween.endValue <= 1) {
-            tween.endValue = 1; 
+            tween.endValue = 1;
             this.removeTween(tween);
           }
         }
@@ -443,13 +477,13 @@ export class Charm {
   }
 
   //3. Motion path tweens
-  
+
   followCurve(
     sprite,
     pointsArray,
-    totalFrames, 
+    totalFrames,
     type = "smoothstep",
-    yoyo = false, 
+    yoyo = false,
     delayBeforeRepeat = 0
   ) {
 
@@ -485,9 +519,9 @@ export class Charm {
     //The `update` method will be called on each frame by the game loop.
     //This is what makes the tween move
     o.update = () => {
-      
-      let normalizedTime, curvedTime, 
-          p = o.pointsArray;
+
+      let normalizedTime, curvedTime,
+        p = o.pointsArray;
 
       if (o.playing) {
 
@@ -499,13 +533,13 @@ export class Charm {
           normalizedTime = o.frameCounter / o.totalFrames;
 
           //Select the correct easing function
-          
+
           //If it's not a spline, use one of the ordinary tween
           //functions
           if (typeArray[0] !== "bounce") {
             curvedTime = this.easingFormulas[type](normalizedTime);
-          } 
-          
+          }
+
           //If it's a spline, use the `spline` function and apply the
           //2 additional `type` array values as the spline's start and
           //end points
@@ -517,18 +551,21 @@ export class Charm {
           //Apply the Bezier curve to the sprite's position 
           sprite.x = this.easingFormulas.cubicBezier(curvedTime, p[0][0], p[1][0], p[2][0], p[3][0]);
           sprite.y = this.easingFormulas.cubicBezier(curvedTime, p[0][1], p[1][1], p[2][1], p[3][1]);
-          
+
           //Add one to the `elapsedFrames`
           o.frameCounter += 1;
         }
 
         //When the tween has finished playing, run the end tasks
         else {
-        o.end(); 
+
+          // make sure we end on the actual end property
+          sprite[property] = o.endValue;
+          o.end();
         }
       }
     };
-      
+
     //The `end` method will be called when the tween is finished
     o.end = () => {
 
@@ -559,21 +596,21 @@ export class Charm {
     o.play = () => {
       o.playing = true;
     };
-    
+
     //Return the tween object
     return o;
   }
 
   walkPath(
-    sprite,                   //The sprite
-    originalPathArray,        //A 2D array of waypoints
-    totalFrames = 300,        //The duration, in frames
-    type = "smoothstep",    //The easing type
-    loop = false,             //Should the animation loop?
-    yoyo = false,             //Shoud the direction reverse?
-    delayBetweenSections = 0  //Delay, in milliseconds, between sections
+    sprite, //The sprite
+    originalPathArray, //A 2D array of waypoints
+    totalFrames = 300, //The duration, in frames
+    type = "smoothstep", //The easing type
+    loop = false, //Should the animation loop?
+    yoyo = false, //Shoud the direction reverse?
+    delayBetweenSections = 0 //Delay, in milliseconds, between sections
   ) {
-    
+
     //Clone the path array so that any possible references to sprite
     //properties are converted into ordinary numbers 
     let pathArray = JSON.parse(JSON.stringify(originalPathArray));
@@ -581,7 +618,7 @@ export class Charm {
     //Figure out the duration, in frames, of each path section by 
     //dividing the `totalFrames` by the length of the `pathArray`
     let frames = totalFrames / pathArray.length;
-    
+
     //Set the current point to 0, which will be the first waypoint
     let currentPoint = 0;
 
@@ -591,26 +628,26 @@ export class Charm {
 
       //Use the `makeTween` function to tween the sprite's
       //x and y position
-      let tween = this.makeTween([ 
+      let tween = this.makeTween([
 
         //Create the x axis tween between the first x value in the
         //current point and the x value in the following point
         [
-          sprite, 
-          "x", 
-          pathArray[currentPoint][0], 
-          pathArray[currentPoint + 1][0], 
-          frames, 
+          sprite,
+          "x",
+          pathArray[currentPoint][0],
+          pathArray[currentPoint + 1][0],
+          frames,
           type
         ],
 
         //Create the y axis tween in the same way
         [
-          sprite, 
-          "y", 
-          pathArray[currentPoint][1], 
-          pathArray[currentPoint + 1][1], 
-          frames, 
+          sprite,
+          "y",
+          pathArray[currentPoint][1],
+          pathArray[currentPoint + 1][1],
+          frames,
           type
         ]
       ]);
@@ -629,8 +666,8 @@ export class Charm {
           this.wait(delayBetweenSections).then(() => {
             tween = makePath(currentPoint);
           });
-        } 
-        
+        }
+
         //If we've reached the end of the path, optionally
         //loop and yoyo it
         else {
@@ -673,18 +710,18 @@ export class Charm {
   }
 
   walkCurve(
-    sprite,                  //The sprite
-    pathArray,               //2D array of Bezier curves
-    totalFrames = 300,       //The duration, in frames
-    type = "smoothstep",     //The easing type
-    loop = false,            //Should the animation loop?
-    yoyo = false,            //Should the direction reverse?
-    delayBeforeContinue = 0  //Delay, in milliseconds, between sections
+    sprite, //The sprite
+    pathArray, //2D array of Bezier curves
+    totalFrames = 300, //The duration, in frames
+    type = "smoothstep", //The easing type
+    loop = false, //Should the animation loop?
+    yoyo = false, //Should the direction reverse?
+    delayBeforeContinue = 0 //Delay, in milliseconds, between sections
   ) {
 
     //Divide the `totalFrames` into sections for each part of the path
     let frames = totalFrames / pathArray.length;
-    
+
     //Set the current curve to 0, which will be the first one
     let currentCurve = 0;
 
@@ -694,7 +731,7 @@ export class Charm {
       //Use the custom `followCurve` function to make
       //a sprite follow a curve
       let tween = this.followCurve(
-        sprite, 
+        sprite,
         pathArray[currentCurve],
         frames,
         type
@@ -709,8 +746,8 @@ export class Charm {
           this.wait(delayBeforeContinue).then(() => {
             tween = makePath(currentCurve);
           });
-        } 
-        
+        }
+
         //If we've reached the end of the path, optionally
         //loop and reverse it
         else {
@@ -742,7 +779,7 @@ export class Charm {
 
     //Make the first path
     let tween = makePath(currentCurve);
-    
+
     //Pass the tween back to the main program
     return tween;
   }
@@ -772,11 +809,11 @@ export class Charm {
 
     //Remove the tween if `tweenObject` doesn't have any nested
     //tween objects
-    if(!tweenObject.tweens) {
+    if (!tweenObject.tweens) {
       tweenObject.pause();
       this.globalTweens.splice(this.globalTweens.indexOf(tweenObject), 1);
-    
-    //Otherwise, remove the nested tween objects
+
+      //Otherwise, remove the nested tween objects
     } else {
       tweenObject.pause();
       tweenObject.tweens.forEach(element => {
@@ -789,7 +826,7 @@ export class Charm {
 
     //Update all the tween objects in the `globalTweens` array
     if (this.globalTweens.length > 0) {
-      for(let i = this.globalTweens.length - 1; i >= 0; i--) {
+      for (let i = this.globalTweens.length - 1; i >= 0; i--) {
         let tween = this.globalTweens[i];
         if (tween) tween.update();
       }
